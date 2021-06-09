e3d1fac0b8bf30ea19ba653e7869c71fed92f50c
吴永波 <yongbo.wu@wondfo.com.cn>
2021-06-10 07:56:18 +0800

    spring boot add pom.xml

---
+++ src/test/java/com/kp/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
